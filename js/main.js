a665626c073bb573fe060d66f0bab6373a6399c8
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-11-21 11:39:03 +0200

    xz

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ jQuery(function ($) {
   $(".MenuBtn").click(function(){
     $(".list").slideToggle();
   });
+  $(".submit-btn").click(function(){
+    $("form").css("display","none");
+    $("table").css("display","block")
+  });
+
 });
 
